353d83cafbeab8de44925c3af025fdb1b5f1e2e1
Matt Zumwalt <matt@databindery.com>
2016-05-18 21:10:48 -0400

    makes test assertions for db tracker.getChanges

--- test/databaseTracker.js
+++ test/databaseTracker.js
@@ -1,25 +1,21 @@
 var test = require('tape')
 var fs = require('fs')
-var Jawn = require('../')
+var DatabaseTracker = require('../bin/databaseTracker')
 var memdb = require('memdb')
 var sqlite3 = require('sqlite3').verbose()
 
 test('retrieve changes', function (t) {
-  var jawn = freshJawn()
-  var file = 'testFile.db'
+  var file = 'testFile.sqlite'
   setUpDatabase(file).then(function () {
-    var dbTracker = jawn.createDatabaseTracker(file)
+    var dbTracker = new DatabaseTracker(file)
     var date = new Date(2016, 4, 13)
     dbTracker.getChanges(date, function (changes) {
-      console.log(changes)
+      t.same(changes.GeoData.length, 1, "only returns changes since date")
+      t.same(changes.GeoData[0].data, 'Geo Data 1', "returns the correct data")
       t.end()
     })
   })
 })
-
-function freshJawn () {
-  return new Jawn({db: memdb()})
-}
 
 function setUpDatabase (file) {
   return new Promise(function (resolve, reject) {
