8e99d463cb90cc22d3086d295f8cc24753b61aeb
Brandon Mellus <mellusbrandon@gmail.com>
2016-06-28 21:52:45 +0000

    Add commit functionality to tracker

--- bin/jawn-database-tracker.js
+++ bin/jawn-database-tracker.js
@@ -1,9 +1,28 @@
 #!/usr/bin/env node
 
 var argv = require('yargs').argv
+var path = require('path')
+var Jawn = require('jawn')
 
 var jawnPath = argv.jawn
 var feedId = argv.feed
 var dbPath = argv.db
+
+var DatabaseTracker = require('./databaseTracker.js')
+var dbTracker = DatabaseTracker(dbPath)
+
+if (argv._[0] === 'commit' && path.extname(jawnPath) === '.jawn') {
+    // Get data from Jawn instance
+    var jawn = retrieveJawnData(jawnPath)
+    
+    dbTracker.getChanges(Date.now(), function (changes) {
+         jawn.importRowsKv(changes, feedId)
+    })
+   
+}
+
+function retrieveJawnData (jawnPath) {
+    return new Jawn({db: jawnPath})
+}
 
 console.log(`You are running the database tracker with Jawn ${jawnPath}, feed ${feedId} and db ${dbPath}`)
--- test/database-tracker-cli.js
+++ test/database-tracker-cli.js
@@ -1,11 +1,13 @@
 const test = require('tape')
 const path = require('path')
+const Jawn = require('jawn')
 // const spawn = require('tape-spawn')
 const pathToTracker = 'node ' + path.resolve(__dirname + '/../bin/jawn-database-tracker.js')
 
+const exec = require('child_process').exec;
+
 test('commit accepts --jawn, --feed and --db args', function (t) {
   // var st = spawn(t, 'jawn-database-tracker commit --jawn /path/to/jawn --feed XXX --db /path/to/db')
-  const exec = require('child_process').exec;
   const command = pathToTracker+' commit --jawn /path/to/jawn --feed XXX --db /path/to/db'
   console.log("Running `"+command+"`")
   exec(command, (error, stdout, stderr) => {
@@ -19,3 +21,23 @@ test('commit accepts --jawn, --feed and --db args', function (t) {
     t.end()
   });
 })
+
+test('commit a change to jawn instance', function (t) {
+  const command = pathToTracker+' commit --jawn data/jawn/data.jawn --feed 1 --db data/sqlite/testFile.sqlite'
+  
+  var jawn = freshJawn()
+  //Make an update to sglite database
+  
+  exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`)
+    // Check to see if changes match test case
+  })
+})
+
+function freshJawn () {
+  return new Jawn()
+}
